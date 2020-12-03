134590709d4e06503ea3b51d39a92f6e55bbee3e
Neil M <neilm813@gmail.com>
2020-12-02 18:17:43 -0800

    initial NewItem component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,51 @@
-import "./App.css";
-import styled from "styled-components";
+import React, { useState } from "react";
+import styled, { createGlobalStyle } from "styled-components";
 
 import { Container } from "./components/Container";
 import { Grid, Row, Col } from "./components/Grid";
+import { NewItem } from "./components/NewItem";
 
 function App() {
+  const [items, setItems] = useState([]);
+
+  // Since this is a small project, otherwise styled-reset pkg could be used.
+  const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+  // To avoid having to both items and setItems as props of NewItem.
+  const addItem = (item) => setItems([item, ...items]);
+
   const Title = styled.h1`
-    font-size: 1.5em;
     text-align: center;
+    margin-bottom: 15px;
+  `;
+
+  const TitleMd = styled.h3`
+    text-align: center;
+    margin-bottom: 15px;
   `;
 
   return (
-    <div>
-      <Container>
-        <Title>Shopping List</Title>
-        <Grid>
-          <Row>
-            <Col size={2}>Hello</Col>
-            <Col size={1}>World</Col>
-          </Row>
-        </Grid>
-      </Container>
-    </div>
+    <Container>
+      <GlobalStyle />
+      <Title>Shopping List</Title>
+      <NewItem addItem={addItem} />
+      <Grid>
+        <Row>
+          <Col size={1}>
+            <TitleMd>Pending Items</TitleMd>
+          </Col>
+          <Col size={1}>
+            <TitleMd>Shopping Cart</TitleMd>
+          </Col>
+        </Row>
+      </Grid>
+    </Container>
   );
 }
 
---
+++ src/components/NewItem.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+import { Grid, Row, ColShadow } from "./Grid";
+import { Input } from "./Input";
+
+export const NewItem = ({ addItem }) => {
+  const defaultQuantity = 1,
+    defaultPrice = 0;
+
+  const [name, setName] = useState("");
+  const [quantity, setQuantity] = useState(defaultQuantity);
+  const [price, setPrice] = useState(defaultPrice);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    addItem({
+      name,
+      quantity: +quantity, // convert to num shorthand
+      price: +price,
+      pending: true,
+    });
+
+    setName("");
+    setQuantity(defaultQuantity);
+    setPrice(defaultPrice);
+  }
+
+  const Title = styled.h4`
+    text-align: center;
+  `;
+
+  const Hr = styled.hr`
+    margin: 10px 0;
+  `;
+
+  const Button = styled.button`
+    font-size: 1em;
+    padding: 0.25em 1em;
+    border: 2px solid lightsteelblue;
+    border-radius: 3px;
+    background: transparent;
+  `;
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Grid>
+        <Row justifyContent="center">
+          <ColShadow>
+            <Title>New Item</Title>
+            <Hr />
+            <label>Name</label>
+            <Input
+              value={name}
+              type="text"
+              onChange={(e) => setName(e.target.value)}
+            />
+
+            <label>Quantity</label>
+            <Input
+              value={quantity}
+              type="number"
+              onChange={(e) => setQuantity(e.target.value)}
+            />
+
+            <label>Price</label>
+            <Input
+              value={price}
+              type="number"
+              onChange={(e) => setPrice(e.target.value)}
+            />
+
+            <div>
+              <Button disabled={name.length < 2}>Submit</Button>
+            </div>
+          </ColShadow>
+        </Row>
+      </Grid>
+    </form>
+  );
+};
