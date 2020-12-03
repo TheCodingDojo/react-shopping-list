3b20e4e8ebb6db29998c7a18241ef287aca4952d
Neil M <neilm813@gmail.com>
2020-12-02 18:16:57 -0800

    add grid styles

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -2,13 +2,20 @@
 import styled from "styled-components";
 
 export const Grid = styled.div``;
+
 export const Row = styled.div`
   display: flex;
   flex-wrap: wrap;
   padding-top: 0.75rem;
   padding-bottom: 0.75rem;
+  justify-content: ${({ justifyContent }) => justifyContent || "left"};
 `;
 
 export const Col = styled.div`
   flex: ${(props) => props.size};
+  padding: 15px;
+`;
+
+export const ColShadow = styled(Col)`
+  box-shadow: 0 0.3rem 0.5rem rgba(0, 0, 0, 0.1);
 `;
