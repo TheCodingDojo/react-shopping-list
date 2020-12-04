0b1364ccd01e502c40e4f68fbab9b17f1c074502
Neil M <neilm813@gmail.com>
2020-12-03 18:06:40 -0800

    conditional edit functionality

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,7 +1,9 @@
+import React, { useState } from "react";
 import styled from "styled-components";
 
 import { WrapperShadow } from "./StyledWrappers";
-import { BtnWarning } from "./StyledButtons";
+import { BtnWarning, BtnPrimary, BtnDefault } from "./StyledButtons";
+import { Input } from "./StyledInputs";
 
 const WrapperShadowMargin = styled(WrapperShadow)`
   margin: 0 5px;
@@ -11,15 +13,82 @@ const TitleLineThrough = styled.h4`
   text-decoration: line-through;
 `;
 
-const Item = ({ item, setItemToEdit }) => {
-  const { name, quantity, price, pending } = item;
+const Item = ({ item, itemToEdit, setItemToEdit, saveEdit }) => {
+  const { name, quantity, price, category, pending } = item;
+
+  const [editedName, setEditedName] = useState(name);
+  const [editedCategory, setEditedCategory] = useState(category);
+  const [editedQuantity, setEditedQuantity] = useState(quantity);
+  const [editedPrice, setEditedPrice] = useState(price);
+
+  function save(e) {
+    e.preventDefault();
+
+    const editedItem = {
+      name: editedName,
+      category: editedCategory,
+      quantity: +editedQuantity || 1,
+      price: +editedPrice || 0,
+    };
+
+    saveEdit(item, editedItem);
+  }
 
   return (
     <WrapperShadowMargin>
-      {pending ? <h4>{name}</h4> : <TitleLineThrough>{name}</TitleLineThrough>}
-      <p>Quantity: {quantity}</p>
-      <p>Price: {price}</p>
-      <BtnWarning onClick={(e) => setItemToEdit(item)}>Edit</BtnWarning>
+      {item === itemToEdit ? (
+        <form onSubmit={save}>
+          <label>Name:</label>
+          <Input
+            value={editedName}
+            onChange={(e) => setEditedName(e.target.value)}
+          />
+
+          <label>Category:</label>
+          <Input
+            value={editedCategory}
+            onChange={(e) => setEditedCategory(e.target.value)}
+          />
+
+          <label>Quantity:</label>
+          <Input
+            value={editedQuantity}
+            onChange={(e) => setEditedQuantity(e.target.value)}
+          />
+
+          <label>Price:</label>
+          <Input
+            value={editedPrice}
+            onChange={(e) => setEditedPrice(e.target.value)}
+          />
+
+          <BtnPrimary
+            disabled={editedName.length <= 1 && editedCategory.length <= 1}
+          >
+            Save
+          </BtnPrimary>
+          <BtnDefault onClick={(e) => setItemToEdit(null)}>Cancel</BtnDefault>
+        </form>
+      ) : (
+        <>
+          {pending ? (
+            <h4>{name}</h4>
+          ) : (
+            <TitleLineThrough>{name}</TitleLineThrough>
+          )}
+          <p>Quantity: {quantity}</p>
+          <p>Price: {price}</p>
+          <BtnWarning
+            onClick={(e) => {
+              // stop the click event from triggering the parent's onClick
+              e.stopPropagation();
+              setItemToEdit(item);
+            }}
+          >
+            Edit
+          </BtnWarning>
+        </>
+      )}
     </WrapperShadowMargin>
   );
 };
