fbf8220de30bfb84eb30830667b6fff58d34f0f7
Neil M <neilm813@gmail.com>
2020-12-03 17:20:48 -0800

    styling

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,6 +3,10 @@ import styled from "styled-components";
 import { WrapperShadow } from "./StyledWrappers";
 import { BtnWarning } from "./StyledButtons";
 
+const WrapperShadowMargin = styled(WrapperShadow)`
+  margin: 0 5px;
+`;
+
 const TitleLineThrough = styled.h4`
   text-decoration: line-through;
 `;
@@ -11,12 +15,12 @@ const Item = ({ item, setItemToEdit }) => {
   const { name, quantity, price, pending } = item;
 
   return (
-    <WrapperShadow>
+    <WrapperShadowMargin>
       {pending ? <h4>{name}</h4> : <TitleLineThrough>{name}</TitleLineThrough>}
       <p>Quantity: {quantity}</p>
       <p>Price: {price}</p>
       <BtnWarning onClick={(e) => setItemToEdit(item)}>Edit</BtnWarning>
-    </WrapperShadow>
+    </WrapperShadowMargin>
   );
 };
 
--- src/components/StyledTitles.js
+++ src/components/StyledTitles.js
@@ -5,15 +5,22 @@ export const Title = styled.h1`
   margin-bottom: 15px;
 `;
 
-export const Title4 = styled.h4`
-  text-align: ${({ center, right }) =>
-    (center && "center") || (right && "right") || "left"};
-`;
-
-export const TitleBorderBot = styled.h3`
+export const Title2BorderBot = styled.h2`
   text-align: center;
   margin-bottom: 15px;
   padding: 1rem;
   border-bottom: 1px solid ${(props) => props.bottomBorderColor || "black"};
   box-shadow: 0 5px 2px -2px rgba(0, 0, 0, 0.075);
+`;
+
+export const TitleCategory = styled.h3`
+  text-align: center;
+  color: ${({ color }) => color};
+  text-decoration: underline;
+  margin-bottom: 10px;
+`;
+
+export const Title4 = styled.h4`
+  text-align: ${({ center, right }) =>
+    (center && "center") || (right && "right") || "left"};
 `;
