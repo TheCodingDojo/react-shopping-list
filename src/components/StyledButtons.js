100374be9f1085b8220401b6a8c7f052ac9f6af1
Neil M <neilm813@gmail.com>
2020-12-03 18:06:50 -0800

    added base btn styles

--- src/components/StyledButtons.js
+++ src/components/StyledButtons.js
@@ -1,23 +1,26 @@
 import styled from "styled-components";
 
-export const BtnPrimary = styled.button`
+export const BtnDefault = styled.button`
   font-size: 1em;
   padding: 0.25em 1em;
-  border: 2px solid slateblue;
+  border: 2px solid gray;
   border-radius: 3px;
   background: transparent;
+  margin-right: 5px;
+  &:hover {
+    background: gray;
+  }
+`;
+
+export const BtnPrimary = styled(BtnDefault)`
+  border-color: slateblue;
   &:hover {
     background: slateblue;
   }
 `;
 
-export const BtnWarning = styled.button`
-  margin-top: 15px;
-  font-size: 1em;
-  padding: 0.25em 1em;
-  border: 2px solid orange;
-  border-radius: 3px;
-  background: transparent;
+export const BtnWarning = styled(BtnDefault)`
+  border-color: orange;
   &:hover {
     background: orange;
   }
